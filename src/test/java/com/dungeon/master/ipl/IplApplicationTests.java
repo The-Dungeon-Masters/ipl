4c723aeaaf32a9581aec9b9f0d731423c3366112
Rhishikesh Magdum <rmagdum@edgewaternetworks.com>
2018-03-23 15:51:21 +0530

    Initial commit

---
+++ src/test/java/com/dungeon/master/ipl/IplApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dungeon.master.ipl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IplApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
