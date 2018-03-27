61b7fd2a4ea6cc256c97904858f509c83fe106a6
Rhishikesh Magdum <rmagdum@edgewaternetworks.com>
2018-03-27 20:55:21 +0530

    Updated db

--- src/main/resources/scripts/insertData.sql
+++ src/main/resources/scripts/insertData.sql
@@ -1,18 +1,19 @@
-INSERT INTO public.users(user_id, user_name, password, email, points)	VALUES (1, 'nakul', 'nakul',null, 100);
-INSERT INTO public.users(user_id, user_name, password, email, points)	VALUES (2, 'yogesh', 'yogesh',null, 200);
+INSERT INTO public.users(user_id, user_name, password, email, points, user_type, created_by)	VALUES (1, 'Superadmin', 'Superadmin',null, 0, 'Admin', 'Default');
 
+INSERT INTO public.user_types(user_types)	VALUES (1, 'Admin');
+INSERT INTO public.user_types(user_types)	VALUES (2, 'User');
 
 INSERT INTO public.teams(team_id, team_name)	VALUES (1, 'MI');
 INSERT INTO public.teams(team_id, team_name)	VALUES (2, 'KKR');
-
+INSERT INTO public.teams(team_id, team_name)	VALUES (1, 'DD');
+INSERT INTO public.teams(team_id, team_name)	VALUES (2, 'CSK');
 
 INSERT INTO public.matches(	match_id, team_one_id, team_two_id, start_time)	VALUES (1, 1, 2, to_timestamp('20-04-2018 15:36:38', 'dd-mm-yyyy hh24:mi:ss'));
 INSERT INTO public.matches(	match_id, team_one_id, team_two_id, start_time)	VALUES (2, 2, 1, to_timestamp('16-04-2018 15:36:38', 'dd-mm-yyyy hh24:mi:ss'));
 
+INSERT INTO public.bid(	bid_id, bid_points, bid_type)	VALUES (1, 0, 'lunch');
+INSERT INTO public.bid(	bid_id, bid_points, bid_type)	VALUES (2, 10, '10 Points');
+INSERT INTO public.bid(	bid_id, bid_points, bid_type)	VALUES (3, 50, '50 Points');
 
-INSERT INTO public.bid(	bid_id, bid_points, bid_type)	VALUES (1, 0, 'niyuj');
-INSERT INTO public.bid(	bid_id, bid_points, bid_type)	VALUES (2, 10, '10 pts');
-INSERT INTO public.bid(	bid_id, bid_points, bid_type)	VALUES (3, 50, '50 pts');
-
-INSERT INTO public.user_matches(	user_id, match_id, team_id, bid_id)	VALUES (1, 1, 2, 2);
-INSERT INTO public.user_matches(	user_id, match_id, team_id, bid_id)	VALUES (2, 1, 2, 3);
+INSERT INTO public.user_matches(user_id, match_id, team_id, bid_id)	VALUES (1, 1, 2, 2);
+INSERT INTO public.user_matches(user_id, match_id, team_id, bid_id)	VALUES (2, 1, 2, 3);
--- src/main/resources/scripts/tablesCreation.sql
+++ src/main/resources/scripts/tablesCreation.sql
@@ -5,7 +5,14 @@ CREATE TABLE if not exists public.users
     password varchar(50) NOT NULL,
     email varchar(50),
     points int NOT NULL,
-    CONSTRAINT users_pkey PRIMARY KEY (user_id)
+    user_type VARCHAR(25) NOT NULL DEFAULT  "User",
+    created_by varchar(50) NOT NULL,
+    CONSTRAINT users_pkey PRIMARY KEY (user_id),
+    CONSTRAINT users_fkey1 FOREIGN KEY (user_type) REFERENCES public.user_types(user_type) ON UPDATE NO ACTION,
+);
+
+CREATE TABLE IF NOT EXISTS public.user_types (
+  user_type VARCHAR(25) UNIQUE
 );
 
 CREATE TABLE if not exists public.teams
@@ -27,24 +34,34 @@ CREATE TABLE if not exists public.matches
     CONSTRAINT matches_fkey2 FOREIGN KEY (team_two_id) REFERENCES public.teams (team_id) ON UPDATE NO ACTION
 );
 
-CREATE TABLE if not exists public.bid
+CREATE TABLE if not exists public.contest
 (
-    bid_id int NOT NULL,
-    bid_points int NOT NULL,
-    bid_type varchar(50) NOT NULL,
-    CONSTRAINT bid_pkey PRIMARY KEY (bid_id)
+    contest_id int NOT NULL,
+    contest_points int NOT NULL,
+    contest_type varchar(50) NOT NULL,
+    CONSTRAINT contest_pkey PRIMARY KEY (contest_id)
 );
 
---DROP TABLE public.user_bid CASCADE;
---CREATE TABLE if not exists public.user_bid
---(
---	
- --   user_id int NOT NULL,
-  --  bid_id int NOT NULL,
-   -- CONSTRAINT user_bid_pkey PRIMARY KEY (user_id,bid_id),
-    --CONSTRAINT user_bid_fkey1 FOREIGN KEY (user_id) REFERENCES public.users (user_id) ON UPDATE NO ACTION,
-    --CONSTRAINT user_bid_fkey2 FOREIGN KEY (bid_id) REFERENCES public.bid (bid_id) ON UPDATE NO ACTION
---);
+CREATE TABLE if not exists public.contest_users
+(
+    id int NOT NULL,
+    contest_id varchar(50) NOT NULL,
+    user_id int NOT NULL,
+    CONSTRAINT contest_users_pkey PRIMARY KEY (id),
+    CONSTRAINT contest_users_fkey1 FOREIGN KEY (contest_id) REFERENCES public.contest (contest_id) ON UPDATE NO ACTION,
+    CONSTRAINT contest_users_fkey2 FOREIGN KEY (user_id) REFERENCES public.users (user_id) ON UPDATE NO ACTION,
+);
+
+CREATE TABLE if not exists public.user_recharge
+(
+    id int NOT NULL,
+    user_id int NOT NULL,
+    recharge_points int NOT NULL,
+    recharged_by varchar(50) NOT NULL,
+    CONSTRAINT contest_users_pkey PRIMARY KEY (id),
+    CONSTRAINT contest_users_fkey1 FOREIGN KEY (user_id) REFERENCES public.users (user_id) ON UPDATE NO ACTION,
+    CONSTRAINT contest_users_fkey2 FOREIGN KEY (recharged_by) REFERENCES public.users (user_name) ON UPDATE NO ACTION,
+);
 
 CREATE TABLE if not exists public.user_matches
 (
@@ -60,20 +77,11 @@ CREATE TABLE if not exists public.user_matches
     CONSTRAINT user_matches_fkey4 FOREIGN KEY (team_id) REFERENCES public.teams (team_id) ON UPDATE NO ACTION
 );
 
---CREATE TABLE if not exists public.result
---(
- --   result_id int NOT NULL,
-  --  result_details varchar(50) NOT NULL,
-   -- CONSTRAINT result_pkey PRIMARY KEY (result_id)
---);
-
---CREATE TABLE if not exists public.match_results
---(
---    match_id int NOT NULL,
---    result_id int NOT NULL,
---    team_id int,
---    CONSTRAINT match_results_pkey PRIMARY KEY (match_id,result_id),
---   	CONSTRAINT match_results_fkey1 FOREIGN KEY (match_id) REFERENCES public.matches (match_id) ON UPDATE NO ACTION,
---   	CONSTRAINT match_results_fkey2 FOREIGN KEY (result_id) REFERENCES public.result (result_id) ON UPDATE NO ACTION,
---   	CONSTRAINT match_results_fkey3 FOREIGN KEY (team_id) REFERENCES public.teams (team_id) ON UPDATE NO ACTION
---);
+CREATE TABLE if not exists public.match_results
+(
+    match_id int NOT NULL,
+    team_id int,
+    CONSTRAINT match_results_pkey PRIMARY KEY (match_id,result_id),
+    CONSTRAINT match_results_fkey1 FOREIGN KEY (match_id) REFERENCES public.matches (match_id) ON UPDATE NO ACTION,
+    CONSTRAINT match_results_fkey3 FOREIGN KEY (team_id) REFERENCES public.teams (team_id) ON UPDATE NO ACTION
+);
