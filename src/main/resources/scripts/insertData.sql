8aede17430f35e75934bf555b36ad8b44c9c9b03
Rhishikesh Magdum <rmagdum@edgewaternetworks.com>
2018-03-28 17:47:49 +0530

    Removed syntax errors and added initial data to db files

--- src/main/resources/scripts/insertData.sql
+++ src/main/resources/scripts/insertData.sql
@@ -1,19 +1,20 @@
-INSERT INTO public.users(user_id, user_name, password, email, points, user_type, created_by)	VALUES (1, 'Superadmin', 'Superadmin',null, 0, 'Admin', 'Default');
+INSERT INTO ipl.user_types(user_type)	VALUES ('Admin');
+INSERT INTO ipl.user_types(user_type)	VALUES ('User');
 
-INSERT INTO public.user_types(user_types)	VALUES (1, 'Admin');
-INSERT INTO public.user_types(user_types)	VALUES (2, 'User');
+INSERT INTO ipl.users(user_id, user_name, password, email, points, user_type, created_by)
+VALUES (1, 'superadmin', 'superadmin','fakePassword', 0, 'Admin', 'Default');
 
-INSERT INTO public.teams(team_id, team_name)	VALUES (1, 'MI');
-INSERT INTO public.teams(team_id, team_name)	VALUES (2, 'KKR');
-INSERT INTO public.teams(team_id, team_name)	VALUES (1, 'DD');
-INSERT INTO public.teams(team_id, team_name)	VALUES (2, 'CSK');
+INSERT INTO ipl.teams(team_id, team_name, team_full_name)	VALUES (1, 'MI', 'MUMBAI INDIANS');
+INSERT INTO ipl.teams(team_id, team_name, team_full_name)	VALUES (2, 'KKR', 'KOLKATA KNIGHT RIDERS');
+INSERT INTO ipl.teams(team_id, team_name, team_full_name)	VALUES (3, 'DD', 'DELHI DAREDEVILS');
+INSERT INTO ipl.teams(team_id, team_name, team_full_name)	VALUES (4, 'CSK', 'CHENNAI SUPER KINGS');
+INSERT INTO ipl.teams(team_id, team_name, team_full_name)	VALUES (5, 'KXIP', 'KINGS XI PUNJAB');
+INSERT INTO ipl.teams(team_id, team_name, team_full_name)	VALUES (6, 'RR', 'RAJASTHAN ROYALS');
+INSERT INTO ipl.teams(team_id, team_name, team_full_name)	VALUES (7, 'RCB', 'ROYAL CHALLENGERS BANGALORE');
+INSERT INTO ipl.teams(team_id, team_name, team_full_name)	VALUES (8, 'SRH', 'SUNRISERS HYDERABAD');
 
-INSERT INTO public.matches(	match_id, team_one_id, team_two_id, start_time)	VALUES (1, 1, 2, to_timestamp('20-04-2018 15:36:38', 'dd-mm-yyyy hh24:mi:ss'));
-INSERT INTO public.matches(	match_id, team_one_id, team_two_id, start_time)	VALUES (2, 2, 1, to_timestamp('16-04-2018 15:36:38', 'dd-mm-yyyy hh24:mi:ss'));
+INSERT INTO ipl.contest(	contest_id, contest_points, contest_type)	VALUES (1, 0, 'lunch');
+INSERT INTO ipl.contest(	contest_id, contest_points, contest_type)	VALUES (2, 10, '10 Points');
+INSERT INTO ipl.contest(	contest_id, contest_points, contest_type)	VALUES (3, 50, '50 Points');
 
-INSERT INTO public.bid(	bid_id, bid_points, bid_type)	VALUES (1, 0, 'lunch');
-INSERT INTO public.bid(	bid_id, bid_points, bid_type)	VALUES (2, 10, '10 Points');
-INSERT INTO public.bid(	bid_id, bid_points, bid_type)	VALUES (3, 50, '50 Points');
 
-INSERT INTO public.user_matches(user_id, match_id, team_id, bid_id)	VALUES (1, 1, 2, 2);
-INSERT INTO public.user_matches(user_id, match_id, team_id, bid_id)	VALUES (2, 1, 2, 3);
--- src/main/resources/scripts/tablesCreation.sql
+++ src/main/resources/scripts/tablesCreation.sql
@@ -1,4 +1,10 @@
-CREATE TABLE if not exists public.users
+CREATE DATABASE if not exists ipl;
+
+CREATE TABLE IF NOT EXISTS ipl.user_types (
+  user_type VARCHAR(25) UNIQUE
+);
+
+CREATE TABLE if not exists ipl.users
 (
     user_id int NOT NULL,
     user_name varchar(50) NOT NULL,
@@ -8,21 +14,18 @@ CREATE TABLE if not exists public.users
     user_type VARCHAR(25) NOT NULL DEFAULT  "User",
     created_by varchar(50) NOT NULL,
     CONSTRAINT users_pkey PRIMARY KEY (user_id),
-    CONSTRAINT users_fkey1 FOREIGN KEY (user_type) REFERENCES public.user_types(user_type) ON UPDATE NO ACTION,
+    CONSTRAINT users_fkey1 FOREIGN KEY (user_type) REFERENCES ipl.user_types(user_type) ON UPDATE NO ACTION
 );
 
-CREATE TABLE IF NOT EXISTS public.user_types (
-  user_type VARCHAR(25) UNIQUE
-);
-
-CREATE TABLE if not exists public.teams
+CREATE TABLE if not exists ipl.teams
 (
     team_id int NOT NULL,
     team_name varchar(50) NOT NULL,
+    team_full_name varchar(50) NOT NULL,
     CONSTRAINT teams_pkey PRIMARY KEY (team_id)
 );
 
-CREATE TABLE if not exists public.matches
+CREATE TABLE if not exists ipl.matches
 (
     match_id int NOT NULL,
     team_one_id int NOT NULL,
@@ -30,11 +33,11 @@ CREATE TABLE if not exists public.matches
     status varchar(50),
     start_time timestamp NOT NULL, 
     CONSTRAINT matches_pkey PRIMARY KEY (match_id),
-    CONSTRAINT matches_fkey1 FOREIGN KEY (team_one_id) REFERENCES public.teams (team_id) ON UPDATE NO ACTION,
-    CONSTRAINT matches_fkey2 FOREIGN KEY (team_two_id) REFERENCES public.teams (team_id) ON UPDATE NO ACTION
+    CONSTRAINT matches_fkey1 FOREIGN KEY (team_one_id) REFERENCES ipl.teams (team_id) ON UPDATE NO ACTION,
+    CONSTRAINT matches_fkey2 FOREIGN KEY (team_two_id) REFERENCES ipl.teams (team_id) ON UPDATE NO ACTION
 );
 
-CREATE TABLE if not exists public.contest
+CREATE TABLE if not exists ipl.contest
 (
     contest_id int NOT NULL,
     contest_points int NOT NULL,
@@ -42,46 +45,46 @@ CREATE TABLE if not exists public.contest
     CONSTRAINT contest_pkey PRIMARY KEY (contest_id)
 );
 
-CREATE TABLE if not exists public.contest_users
+CREATE TABLE if not exists ipl.contest_users
 (
     id int NOT NULL,
-    contest_id varchar(50) NOT NULL,
+    contest_id int NOT NULL,
     user_id int NOT NULL,
     CONSTRAINT contest_users_pkey PRIMARY KEY (id),
-    CONSTRAINT contest_users_fkey1 FOREIGN KEY (contest_id) REFERENCES public.contest (contest_id) ON UPDATE NO ACTION,
-    CONSTRAINT contest_users_fkey2 FOREIGN KEY (user_id) REFERENCES public.users (user_id) ON UPDATE NO ACTION,
+    CONSTRAINT contest_users_fkey1 FOREIGN KEY (contest_id) REFERENCES ipl.contest (contest_id) ON UPDATE NO ACTION,
+    CONSTRAINT contest_users_fkey2 FOREIGN KEY (user_id) REFERENCES ipl.users (user_id) ON UPDATE NO ACTION
 );
 
-CREATE TABLE if not exists public.user_recharge
+CREATE TABLE if not exists ipl.user_recharge
 (
     id int NOT NULL,
     user_id int NOT NULL,
     recharge_points int NOT NULL,
-    recharged_by varchar(50) NOT NULL,
-    CONSTRAINT contest_users_pkey PRIMARY KEY (id),
-    CONSTRAINT contest_users_fkey1 FOREIGN KEY (user_id) REFERENCES public.users (user_id) ON UPDATE NO ACTION,
-    CONSTRAINT contest_users_fkey2 FOREIGN KEY (recharged_by) REFERENCES public.users (user_name) ON UPDATE NO ACTION,
+    recharged_by int NOT NULL,
+    CONSTRAINT user_recharge_pkey PRIMARY KEY (id),
+    CONSTRAINT user_recharge_fkey1 FOREIGN KEY (user_id) REFERENCES ipl.users (user_id) ON UPDATE NO ACTION,
+    CONSTRAINT user_recharge_fkey2 FOREIGN KEY (recharged_by) REFERENCES ipl.users (user_id) ON UPDATE NO ACTION
 );
 
-CREATE TABLE if not exists public.user_matches
+CREATE TABLE if not exists ipl.user_matches
 (
     user_id int NOT NULL,
+    contest_id int NOT NULL,
     match_id int NOT NULL,
     team_id int NOT NULL,
-    bid_id int NOT NULL,
     points int,
-    CONSTRAINT user_matches_pkey PRIMARY KEY (user_id,match_id,team_id,bid_id),
-    CONSTRAINT user_matches_fkey1 FOREIGN KEY (user_id) REFERENCES public.users (user_id) ON UPDATE NO ACTION,
-    CONSTRAINT user_matches_fkey2 FOREIGN KEY (bid_id) REFERENCES public.bid (bid_id) ON UPDATE NO ACTION,
-	CONSTRAINT user_matches_fkey3 FOREIGN KEY (match_id) REFERENCES public.matches (match_id) ON UPDATE NO ACTION,
-    CONSTRAINT user_matches_fkey4 FOREIGN KEY (team_id) REFERENCES public.teams (team_id) ON UPDATE NO ACTION
+    CONSTRAINT user_matches_pkey PRIMARY KEY (user_id,match_id,team_id,contest_id),
+    CONSTRAINT user_matches_fkey1 FOREIGN KEY (user_id) REFERENCES ipl.users (user_id) ON UPDATE NO ACTION,
+    CONSTRAINT user_matches_fkey2 FOREIGN KEY (contest_id) REFERENCES ipl.contest (contest_id) ON UPDATE NO ACTION,
+    CONSTRAINT user_matches_fkey3 FOREIGN KEY (match_id) REFERENCES ipl.matches (match_id) ON UPDATE NO ACTION,
+    CONSTRAINT user_matches_fkey4 FOREIGN KEY (team_id) REFERENCES ipl.teams (team_id) ON UPDATE NO ACTION
 );
 
-CREATE TABLE if not exists public.match_results
+CREATE TABLE if not exists ipl.match_results
 (
     match_id int NOT NULL,
     team_id int,
-    CONSTRAINT match_results_pkey PRIMARY KEY (match_id,result_id),
-    CONSTRAINT match_results_fkey1 FOREIGN KEY (match_id) REFERENCES public.matches (match_id) ON UPDATE NO ACTION,
-    CONSTRAINT match_results_fkey3 FOREIGN KEY (team_id) REFERENCES public.teams (team_id) ON UPDATE NO ACTION
+    CONSTRAINT match_results_pkey PRIMARY KEY (match_id),
+    CONSTRAINT match_results_fkey1 FOREIGN KEY (match_id) REFERENCES ipl.matches (match_id) ON UPDATE NO ACTION,
+    CONSTRAINT match_results_fkey3 FOREIGN KEY (team_id) REFERENCES ipl.teams (team_id) ON UPDATE NO ACTION
 );
